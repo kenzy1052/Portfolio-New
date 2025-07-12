2dcf0cb19b56f3bfacfe5a2b5290b79908d3b98d
Kenzy1052 <kenzy1052@gmail.com>
2025-07-12 01:39:23 +0000

    Changes to the portfolio link.

--- assets/src/portfolio.js
+++ assets/src/portfolio.js
@@ -5,6 +5,7 @@ const modalTitle = document.querySelector(".modal-title");
 const modalDescription = document.querySelector(".modal-description");
 const modalImage = document.querySelector(".image-modal");
 const closeBtn = document.querySelector(".closebtn");
+const modalButton = document.querySelector(".cta-buttons");
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
@@ -12,6 +13,28 @@ cards.forEach((card) => {
     const subtitle = card.getAttribute("data-subtitle");
     const title = card.getAttribute("data-title");
     const description = card.getAttribute("data-description");
+
+    if (title === "First Portfolio") {
+      modalButton.innerHTML = `<a href="./Portfolio/index.html" target="_blank">
+        <button class="viewbtn btn">
+          View
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2.5"
+            stroke="currentColor"
+            class="size-6 icon-rose"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
+      </a>`;
+    }
 
     modalImage.src = img;
     modalSubtitle.textContent = subtitle;
